8fab2a2048cc9741c18d2d2c9afcd1888d67863b
guols <guols@eternalinfo.cn>
2018-06-06 16:50:31 +0800

    test11

--- aaa/testgit/src/testgit/TestMain.java
+++ aaa/testgit/src/testgit/TestMain.java
@@ -4,8 +4,9 @@ public class TestMain {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
-		System.out.println("nnnnnnnnnnnnn");
-		System.out.println("meiguo");
+		System.out.println("1111111111111");
+		System.out.println("2222222222222");
+		System.out.println("dddd");
 	}
 
 }
