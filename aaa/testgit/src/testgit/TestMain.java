ca7a144b7c48a12e54b51fa13d47fbf5c724db65
guols <guols@eternalinfo.cn>
2018-06-06 16:25:39 +0800

    test6

--- aaa/testgit/src/testgit/TestMain.java
+++ aaa/testgit/src/testgit/TestMain.java
@@ -5,7 +5,7 @@ public class TestMain {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		System.out.println("aaaaaaaaaaaaaaa");
-		System.out.println("testuser");
+		System.out.println("meiguo");
 	}
 
 }
