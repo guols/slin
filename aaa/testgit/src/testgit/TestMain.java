d6ce09acf2878bc5526ce8b32774bd38172d5e0a
guols <guols@eternalinfo.cn>
2018-06-07 11:38:07 +0800

    111

--- aaa/testgit/src/testgit/TestMain.java
+++ aaa/testgit/src/testgit/TestMain.java
@@ -5,16 +5,8 @@ public class TestMain {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("44444444444444444");
-        System.out.println("55666666666655");
-        System.out.println("66777776666");
-        System.out.println("epppppppppnd");
         System.out.println("99998888889999");
 
-    }
-
-    protected static void jiami() {
-        System.out.println("中国");
-        System.out.println("美国");
     }
 
     protected static void commons() {
@@ -22,10 +14,6 @@ public class TestMain {
         d = Integer.parseInteger("3");
 
         System.out.println(d);
-    }
-
-    private static void jiami() {
-        System.out.println("加密码成功！");
     }
 
 }
