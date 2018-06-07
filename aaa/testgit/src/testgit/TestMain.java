92b9fd6687171f995d967625ec31265f346edb27
guols <guols@eternalinfo.cn>
2018-06-07 11:49:48 +0800

    111

--- aaa/testgit/src/testgit/TestMain.java
+++ aaa/testgit/src/testgit/TestMain.java
@@ -9,14 +9,13 @@ public class TestMain {
 
     }
 
-    private static void jiami() {
+    private static void jiami333333() {
         System.out.println("加密111111111码！");
     }
 
     protected static void commons() {
         int d = 0;
         d = Integer.parseInteger("3");
-        System.out.println("dsdddd");
         System.out.println(d);
     }
 
