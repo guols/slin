ea5d4a52c4c4ac63bf90a6de473205d7195c7c0e
guols <guols@eternalinfo.cn>
2018-06-07 09:33:56 +0800

    111

--- aaa/testgit/src/testgit/TestMain.java
+++ aaa/testgit/src/testgit/TestMain.java
@@ -5,8 +5,8 @@ public class TestMain {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		System.out.println("1111111111111");
-		System.out.println("2222222222222");
-		System.out.println("44444444444444444");
+		System.out.println("22222333332222");
+		System.out.println("444444444");
 		System.out.println("55666666666655");
 		System.out.println("66666666666666");
 	}
