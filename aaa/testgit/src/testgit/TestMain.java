92d9d53f666be2f0c46e954a85329698d5ad8156
guols <guols@eternalinfo.cn>
2018-06-07 09:13:01 +0800

    2222

--- aaa/testgit/src/testgit/TestMain.java
+++ aaa/testgit/src/testgit/TestMain.java
@@ -6,9 +6,8 @@ public class TestMain {
 		System.out.println("Hello world!");
 		System.out.println("1111111111111");
 		System.out.println("2222222222222");
-		
 		System.out.println("44444444444444444");
-		
+		System.out.println("5555");
 	}
 
 }
